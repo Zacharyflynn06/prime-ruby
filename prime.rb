bf7be9556a7f8c4b7c3c80c19a6e270af305a95d
Zac <zacharyflynn06@gmail.com>
2020-10-30 18:23:21 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(num)
+    if num <= 1
+      return false
+    else 
+        if (2...num).any? { |x| num % x  == 0}
+            return false
+        else
+            return true
+        end 
+    end
+end
